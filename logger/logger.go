0ce31ebcf9038e2f211872e42940b86be3a4c62b
Yrvin Escorihuela <yrvin.escorihuela@gmail.com>
2022-01-15 23:06:33 -0300

    Define and refactor logger

--- logger/logger.go
+++ logger/logger.go
@@ -1,1 +1,115 @@
 package logger
+
+import (
+	"encoding/json"
+	"io"
+	"os"
+	"runtime/debug"
+	"sync"
+	"time"
+)
+
+type Level int8
+
+const (
+	LevelTrace Level = iota
+	LevelDebug
+	LevelInfo
+	LevelWarning
+	LevelError
+	LevelFatal
+	LevelOff
+)
+
+const (
+	TRACE   string = "TRACE"
+	DEBUG   string = "DEBUG"
+	INFO    string = "INFO"
+	WARNING string = "WARNING"
+	FATAL   string = "FATAL"
+	ERROR   string = "ERROR"
+	DEFAULT string = ""
+)
+
+type Logger struct {
+	out      io.Writer
+	minLevel Level
+	mu       sync.Mutex
+}
+
+func (level Level) String() string {
+	switch level {
+	case LevelTrace:
+		return TRACE
+	case LevelDebug:
+		return DEBUG
+	case LevelInfo:
+		return INFO
+	case LevelWarning:
+		return WARNING
+	case LevelFatal:
+		return FATAL
+	case LevelError:
+		return ERROR
+	default:
+		return DEFAULT
+	}
+}
+
+func New(out io.Writer, minLevel Level) *Logger {
+	return &Logger{
+		out:      out,
+		minLevel: minLevel,
+	}
+}
+
+func (l *Logger) PrintInfo(message string, properties map[string]string) {
+	l.print(LevelInfo, message, properties)
+}
+
+func (l *Logger) PrintError(err error, properties map[string]string) {
+	l.print(LevelError, err.Error(), properties)
+}
+
+func (l *Logger) PrintFatal(err error, properties map[string]string) {
+	l.print(LevelFatal, err.Error(), properties)
+	os.Exit(1)
+}
+
+func (l *Logger) print(level Level, message string, properties map[string]string) (int, error) {
+	if level < l.minLevel {
+		return 0, nil
+	}
+	aux := struct {
+		Level      string            `json:"level"`
+		Time       string            `json:"time"`
+		Message    string            `json:"message"`
+		Properties map[string]string `json:"properties,omitempty"`
+		Trace      string            `json:"trace,omitempty"`
+	}{
+		Level:      level.String(),
+		Time:       time.Now().UTC().Format(time.RFC3339),
+		Message:    message,
+		Properties: properties,
+	}
+
+	if level >= LevelError {
+		aux.Trace = string(debug.Stack())
+	}
+
+	var line []byte
+
+	line, err := json.Marshal(aux)
+	if err != nil {
+		line = []byte(LevelError.String() + ": unabled to marshal log message: " + err.Error())
+	}
+
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	return l.out.Write(append(line, '\n'))
+}
+
+func (l *Logger) Write(message []byte) (n int, err error) {
+	return l.print(LevelError, string(message), nil)
+}
