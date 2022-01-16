5d2d7b17bb8fd732b681e06066e9b8d53fe04263
Yrvin Escorihuela <yrvin.escorihuela@gmail.com>
2022-01-16 18:59:52 -0300

    Add new values for environment and seeding mongodb

--- resources/database/products-db-master/database/mongodb.go
+++
@@ -1,7 +0,0 @@
-package database
-
-import "go.mongodb.org/mongo-driver/mongo"
-
-func NewMongoConnection() *mongo.Client {
-	return nil
-}
--- resources/database/products-db-master/mongo-init-admin-user.js
+++
@@ -1,8 +0,0 @@
-use admin
-db.createUser(
-  {
-    user: "productListUser",
-    pwd: "productListPassword",
-    roles: [ { role: "userAdminAnyDatabase", db: "admin" }, "readWriteAnyDatabase" ]
-  }
-)
