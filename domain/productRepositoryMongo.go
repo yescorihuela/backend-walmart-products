1c17592f45b2d2930c5de913908a4ec6688bb8af
Yrvin Escorihuela <yrvin.escorihuela@gmail.com>
2022-01-16 22:38:53 -0300

    Add custom error messages and refactor add OR condition to searching

--- domain/productRepositoryMongo.go
+++ domain/productRepositoryMongo.go
@@ -1,15 +1,78 @@
 package domain
 
-type ProductRepositoryMongo struct{}
+import (
+	"context"
+	"log"
+	"strconv"
+	"time"
 
-func (prm ProductRepositoryMongo) GetAllProducts() ([]Product, error) {
+	"github.com/yescorihuela/walmart-products/errs"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type ProductRepositoryMongo struct {
+	client *mongo.Client
+}
+
+func (prm ProductRepositoryMongo) GetAllProducts() ([]Product, *errs.AppError) {
+	var products []Product
+	collection := prm.client.Database("promotions").Collection("products")
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cursor.Close(ctx)
+	if err = cursor.All(ctx, &products); err != nil {
+		log.Fatal(err)
+	}
+
+	if len(products) == 0 {
+		return nil, errs.NewNotFoundError("Elements not found")
+	}
+
+	return products, nil
+}
+
+func (prm ProductRepositoryMongo) GetProduct(criteria string) (*Product, *errs.AppError) {
 	return nil, nil
 }
 
-func (prm ProductRepositoryMongo) GetProduct(criteria string) (*Product, error) {
-	return nil, nil
+func (prm ProductRepositoryMongo) GetProductsByCriteria(criteria string) ([]Product, *errs.AppError) {
+	var products []Product
+	collection := prm.client.Database("promotions").Collection("products")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+	var searchingCriteria bson.M
+
+	id, errId := strconv.Atoi(criteria)
+	if errId == nil {
+		searchingCriteria = bson.M{"id": id}
+	} else {
+		var orQuery []map[string]interface{}
+		orQuery = append(orQuery, bson.M{"brand": criteria}, bson.M{"description": criteria})
+		searchingCriteria = bson.M{"$or": orQuery}
+	}
+
+	productsByCriteria, err := collection.Find(ctx, searchingCriteria)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer productsByCriteria.Close(ctx)
+	if err = productsByCriteria.All(ctx, &products); err != nil {
+		log.Fatal(err)
+	}
+
+	if len(products) == 0 {
+		return nil, errs.NewNotFoundError("Elements not found")
+	}
+
+	return products, nil
 }
 
-func NewProductRepositoryMongo(db *interface{}) ProductRepositoryMongo {
-	return ProductRepositoryMongo{}
+func NewProductRepositoryMongo(db *mongo.Client) ProductRepositoryMongo {
+	return ProductRepositoryMongo{client: db}
 }
