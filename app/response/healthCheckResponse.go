6818fcf21a41eecf70753104738a021dafe523c8
Yrvin Escorihuela <yrvin.escorihuela@gmail.com>
2022-01-16 22:51:00 -0300

    Convert to exportable struct members

--- app/response/healthCheckResponse.go
+++ app/response/healthCheckResponse.go
@@ -1,6 +1,6 @@
 package response
 
 type healthz struct {
-	serverStatus   string `json:"server_status"`
-	databaseStatus string `json:"database_status"`
+	ServerStatus   string `json:"server_status"`
+	DatabaseStatus string `json:"database_status"`
 }
